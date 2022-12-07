846160c6eea8336f4bb4219941fa7d1def05b85d
Dennis van Valderen <dennisvanvalderen@gmail.com>
2022-12-07 15:55:30 +0100

    added everything for scss styling

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss'],
+})
+export class ItemComponent {
+  @Input()
+  number: number = 1;
+}
